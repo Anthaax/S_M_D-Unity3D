ae3f644e615d255f845f587f88d61e82f0007483
Anthaax <guillaume.fimes@laposte.net>
2016-05-10 16:11:21 +0200

    Dll avec

---
+++ Assets/Script/TEstDll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using S_M_D.Character;
+using System.Collections.Generic;
+
+public class TEstDll : MonoBehaviour {
+    string _name;
+	// Use this for initialization
+	void Start () {
+        InitializedHero();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void InitializedHero()
+    {
+        List<BaseHeros> myHero = new List<BaseHeros>();
+        HerosManager hm = new HerosManager(myHero);
+        name = hm.Find( HerosEnum.Warrior.ToString() ).CharacterName;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label( _name );
+    }
+}
