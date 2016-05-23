97d1575ef7222b3308debc3273ffef116b83c9b4
JorisNT <jorisnsenguet@gmail.com>
2016-05-23 09:48:37 +0200

    DragAndDropScript && HeroesNewsScript

--- Assets/Scripts/DragAndDropScript.cs
+++ Assets/Scripts/DragAndDropScript.cs
@@ -1,15 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.ParticularName;
+using UnityEngine.UI;
 
 public class DragAndDropScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+    private Vector2 oldPosition;
+
+    void Update()
+    {
+        if (HasInput)
+        {
+            DragOrPickUp();
+        }
+        else
+        {
+            if (draggingItem)
+                DropItem();
+        }
+    }
+
+    Vector2 CurrentTouchPosition
+    {
+        get
+        {
+            Vector2 inputPos;
+            inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return inputPos;
+        }
+    }
+
+    private void DragOrPickUp()
+    {
+        var inputPosition = CurrentTouchPosition;
+
+        if (draggingItem)
+        {
+            draggedObject.transform.position = inputPosition + touchOffset;
+        }
+        else
+        {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0)
+            {
+                var hit = touches[0];
+                if (hit.transform != null && hit.transform.gameObject.tag == TagName.pHero.ToString())
+                {
+                    draggingItem = true;
+                    draggedObject = hit.transform.gameObject;
+                    oldPosition = draggedObject.transform.position;
+                    touchOffset = (Vector2)hit.transform.position - inputPosition;
+                }
+            }
+        }
+    }
+
+    private bool HasInput
+    {
+        get
+        {
+            // returns true if either the mouse button is down or at least one touch is felt on the screen
+            return Input.GetMouseButton(0);
+        }
+    }
+
+    void DropItem()
+    {
+        draggingItem = false;
+        Debug.Log("Left !!!");
+        //draggedObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        draggedObject.transform.position = oldPosition;
+    }
 }
