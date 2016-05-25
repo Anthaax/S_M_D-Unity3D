f44e9148ef798602f0ac1b5bae486f4e02f7f79c
synouk <crousseau@intechinfo.fr>
2016-05-25 10:11:57 +0200

    Remise en forme oklm

--- Assets/Scripts/Campementv2/Profil/SetProfil.cs
+++ Assets/Scripts/Campementv2/Profil/SetProfil.cs
@@ -1,31 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using S_M_D.Character;
 
 public class SetProfil : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (BaseCampement.ActivHeros !=null)
-        {
-            if (BaseCampement.ActivBuilding==null)
-            {
-                GameObject.Find("Icone").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + BaseCampement.ActivHeros.CharacterClassName + "IconeF");
-                GameObject.Find("AttackT").GetComponent<Text>().text = BaseCampement.ActivHeros.EffectivDamage.ToString();
-                GameObject.Find("ArmorT").GetComponent<Text>().text = BaseCampement.ActivHeros.EffectivDefense.ToString();
-                GameObject.Find("SpeedT").GetComponent<Text>().text = BaseCampement.ActivHeros.EffectivSpeed.ToString();
-
-                if (BaseCampement.ActivHeros.InBuilding == null)
-                    GameObject.Find("ArmoryHero").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + BaseCampement.ActivHeros.CharacterClassName + "IconeF");
-
-            }
-
-        }
-        
+    public void Show()
+    {
+        string name = gameObject.name;
+        int index = int.Parse(""+name[name.Length - 2]);
+        BaseHeros heros = Start.Gtx.PlayerInfo.MyHeros[index - 1];
+        GameObject.Find("Icone").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + heros.CharacterClassName + "IconeF");
     }
 }
---
+++ Assets/Scripts/Campementv2/Start.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using S_M_D.Character;
+using S_M_D.Camp;
+using S_M_D;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Start : MonoBehaviour {
+
+    private static GameContext _gtx;
+    private static List<BaseHeros> _characterList;
+    private static List<BaseBuilding> _buildingList;
+
+
+    // Use this for initialization
+    void Awake () {
+
+         Gtx = GameContext.CreateNewGame();
+        CharacterList = Gtx.PlayerInfo.MyHeros;
+        BuildingList = Gtx.PlayerInfo.MyBuildings;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int x = 1;
+        foreach (BaseHeros heros in CharacterList)
+        {
+            GameObject.Find("Hero" + x + "T").GetComponent<Text>().text = heros.CharacterName;
+            GameObject.Find("Hero" + x + "I").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + heros.CharacterClassName + "IconeF");
+            x++;
+
+        }
+	}
+
+    public static GameContext Gtx
+    {
+        get
+        {
+            return _gtx;
+        }
+
+        set
+        {
+            _gtx = value;
+        }
+    }
+
+    public static List<BaseBuilding> BuildingList
+    {
+        get
+        {
+            return _buildingList;
+        }
+
+        set
+        {
+            _buildingList = value;
+        }
+    }
+
+    public static List<BaseHeros> CharacterList
+    {
+        get
+        {
+            return _characterList;
+        }
+
+        set
+        {
+            _characterList = value;
+        }
+    }
+}
