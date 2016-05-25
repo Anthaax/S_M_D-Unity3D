a367e69f37aba0c7a163399781dcbc201ca11526
Peuch <lucas.78.13@gmail.com>
2016-05-25 09:31:49 +0200

    Speed

--- Assets/Scripts/Campementv2/Profil/SetProfil.cs
+++ Assets/Scripts/Campementv2/Profil/SetProfil.cs
@@ -16,7 +16,9 @@ public class SetProfil : MonoBehaviour {
             if (BaseCampement.ActivBuilding==null)
             {
                 GameObject.Find("Icone").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + BaseCampement.ActivHeros.CharacterClassName + "IconeF");
+                GameObject.Find("AttackT").GetComponent<Text>().text = BaseCampement.ActivHeros.EffectivDamage.ToString();
                 GameObject.Find("ArmorT").GetComponent<Text>().text = BaseCampement.ActivHeros.EffectivDefense.ToString();
+                GameObject.Find("SpeedT").GetComponent<Text>().text = BaseCampement.ActivHeros.EffectivSpeed.ToString();
 
                 if (BaseCampement.ActivHeros.InBuilding == null)
                     GameObject.Find("ArmoryHero").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + BaseCampement.ActivHeros.CharacterClassName + "IconeF");
