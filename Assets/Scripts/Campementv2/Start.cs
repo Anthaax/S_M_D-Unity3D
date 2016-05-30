bccb04d7f24a3220f51b8eff6cb5885cd08999d6
Peuch <lucas.78.13@gmail.com>
2016-05-30 15:47:00 +0200

    Corrections + item armory

--- Assets/Scripts/Campementv2/Start.cs
+++ Assets/Scripts/Campementv2/Start.cs
@@ -24,6 +24,7 @@ public class Start : MonoBehaviour {
 
     public static Button[] ButtonsSicknesses;
     public static Button[] ButtonsMentalPsycho;
+    public static Button[] ButtonsArmor;
     // Use this for initialization
     void Awake () {
 
@@ -55,6 +56,8 @@ public class Start : MonoBehaviour {
 
         ButtonsMentalPsycho = MenuBGMentalhospital.GetComponentsInChildren<Button>();
         MentalHospitalBoard.InitializedButtonsMentalHospitalBoard();
+
+        ButtonsArmor = MenuBGArmory.GetComponentsInChildren<Button>();
 
         Caravan caravan = Start.Gtx.PlayerInfo.GetBuilding(S_M_D.Camp.Class.BuildingNameEnum.Caravan) as Caravan;
         caravan.Initialized();
