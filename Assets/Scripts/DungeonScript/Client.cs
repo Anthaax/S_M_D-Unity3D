8d683fa5cc342d073b57d4c8a98d5565e9ead39c
Benoit Humblot <benoit.humblot@orange.fr>
2016-06-02 10:23:57 +0200

    Ajout des Ressources de toutes l'exploration du donjon

---
+++ Assets/Scripts/DungeonScript/Client.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+    private HostData[ ] hostList;
+
+	// Use this for initialization
+	void Start ()
+    {
+        MasterServer.ipAddress = "92.103.197.250";
+        MasterServer.RequestHostList( "Synouk_must_die" );
+	}
+	
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if ( msEvent == MasterServerEvent.HostListReceived )
+        {
+            hostList = MasterServer.PollHostList( );
+            Debug.Log( "Connecté!" );
+        }
+            
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
