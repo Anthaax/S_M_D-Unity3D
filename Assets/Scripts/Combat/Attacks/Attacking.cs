f35a5a1744062132cc7b758d3125db6b35967e08
Anthaax <guillaume.fimes@laposte.net>
2016-06-24 11:13:57 +0200

    Changement dans  les attques des monstres

--- Assets/Scripts/Combat/Attacks/Attacking.cs
+++ Assets/Scripts/Combat/Attacks/Attacking.cs
@@ -8,42 +8,25 @@ using System.Collections.Generic;
 using S_M_D.Spell;
 using System.Linq;
 
-public class Attacking : MonoBehaviour {
+public class Attacking : MonoBehaviour
+{
 
     public void OnClick()
     {
-        if (BaseCombat.Attack.Spell != null && BaseCombat.Attack.Target >= 0) 
+        if (BaseCombat.Attack.Spell != null && BaseCombat.Attack.Target >= 0)
         {
 
-            StartCoroutine(Movement());
-            if (BaseCombat.Attack.Spell.KindOfEffect.DamageType == DamageTypeEnum.Move)
-                MoveAction();
-            else
-            {
-                Debug.Log( "Monstre n°" + BaseCombat.Attack.Target );
-                BaseCombat.Combat.SpellManager.HeroLaunchSpell( BaseCombat.Attack.Spell, BaseCombat.Attack.Target );
-                BaseCombat.Combat.NextTurn();
-            }
+            StartCoroutine( Movement() );
+
+            Debug.Log( "Monstre n°" + BaseCombat.Attack.Target );
+            BaseCombat.Combat.SpellManager.HeroLaunchSpell( BaseCombat.Attack.Spell, BaseCombat.Attack.Target );
+            BaseCombat.Combat.NextTurn();
             BaseCombat.Attack.Target = -1;
-            Timer T = GameObject.Find("Timer").GetComponent<Timer>();
+            Timer T = GameObject.Find( "Timer" ).GetComponent<Timer>();
             T.timeLeft = 30.0f;
 
         }
-
-        GameObject.Find("SpellInfo").GetComponent<Text>().text = "";
-        for (int i =1; i<5; i++)
-        {
-            if (GameObject.Find("Arrow"+i) != null )
-            {
-                GameObject.Find("Arrow"+i).GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Combat/1024px-Red_Arrow_Down.svg");
-                GameObject.Find("Arrow" + i).GetComponent<Image>().enabled = false;
-            }
-            else if (BaseCombat.Attack.Spell.KindOfEffect.DamageType == DamageTypeEnum.Heal || BaseCombat.Attack.Spell.KindOfEffect.DamageType == DamageTypeEnum.Move)
-            {
-                //ajout de fleche pour les heros
-            }
-        }
-       
+        AfterAction();
     }
 
     void Update()
@@ -53,13 +36,12 @@ public class Attacking : MonoBehaviour {
         if (monster != null)
         {
             StartCoroutine( MonsterAttack( monster ) );
-            BaseCombat.Combat.NextTurn();
             Timer T = GameObject.Find( "Timer" ).GetComponent<Timer>();
             T.timeLeft = 30.0f;
         }
 
 
-        
+
     }
 
     public IEnumerator Movement()
@@ -75,51 +57,40 @@ public class Attacking : MonoBehaviour {
             }
             x++;
         }
-        Animator animator = GameObject.Find("Heros" + y).GetComponent<Animator>();
-        animator.Play(H.CharacterClassName+"Attack");
-        yield return new WaitForSeconds(2);
-        animator.Play(H.CharacterClassName + "Idle");
+        Animator animator = GameObject.Find( "Heros" + y ).GetComponent<Animator>();
+        animator.Play( H.CharacterClassName + "Attack" );
+        yield return new WaitForSeconds( 2 );
+        animator.Play( H.CharacterClassName + "Idle" );
 
     }
 
-    public IEnumerator MonsterAttack(BaseMonster Monster)
+    public IEnumerator MonsterAttack( BaseMonster Monster )
     {
-        Debug.Log( Monster.Spells.Count );
-        List<Spells> canLauncSpell = Monster.Spells
-                                    .Where( c => c.TargetManager.WhoCanBeTargetable( Monster.Position ) != new bool[4] { false, false, false, false } )
-                                    .Where( c => c.CooldownManager.IsOnCooldown == false )
-                                    .ToList();
-        Debug.Log( canLauncSpell.Count );
-        if (canLauncSpell.Count > 0)
-        {
-            Spells spellToLaunch = canLauncSpell[BaseCombat.Combat.GameContext.Rnd.Next( canLauncSpell.Count )];
-            int position = BaseCombat.Combat.GameContext.Rnd.Next( 4 );
-            BaseCombat.Combat.SpellManager.MonsterLaunchSpell( spellToLaunch, position );
-            GameObject.Find( "SpellsAttack" ).GetComponent<Text>().text = spellToLaunch.Description;
-        }
-            
+        BaseCombat.Combat.IaMonster.MonsterTurnAndDoNextTurn(Monster);
+        GameObject.Find( "SpellsAttack" ).GetComponent<Text>().text = MonsterAction();
+        BaseCombat.Combat.IaMonster.MosterAction.Clear();
+
         yield return new WaitForSeconds( 1 );
-        
+
         GameObject.Find( "SpellsAttack" ).GetComponent<Text>().text = "";
-        
-
     }
-
-    public void MoveAction()
+    public static void AfterAction()
     {
-        BaseHeros hero = BaseCombat.Combat.GetCharacterTurn() as BaseHeros;
-        BaseHeros hero2 = BaseCombat.Combat.Heros[BaseCombat.Attack.Target];
-
-        BaseCombat.Combat.SpellManager.MoveCharacter<BaseHeros>( GetPositionOfTheHero( hero ), BaseCombat.Attack.Target );
-    }
-
-    private int GetPositionOfTheHero( BaseHeros hero )
-    {
-        for (int i = 0; i < BaseCombat.Combat.Heros.Length; i++)
+        GameObject.Find( "SpellInfo" ).GetComponent<Text>().text = "";
+        for (int i = 1; i < 5; i++)
         {
-            if (hero == BaseCombat.Combat.Heros[i])
-                return i;
+            if (GameObject.Find( "Arrow" + i ) != null)
+            {
+                GameObject.Find( "Arrow" + i ).GetComponent<Image>().sprite = Resources.Load<Sprite>( "Sprites/Combat/1024px-Red_Arrow_Down.svg" );
+                GameObject.Find( "Arrow" + i ).GetComponent<Image>().enabled = false;
+            }
         }
-        return 0;
+    }
+    private string MonsterAction()
+    {
+        if (BaseCombat.Combat.IaMonster.MosterAction.Count != 0)
+            return BaseCombat.Combat.IaMonster.MosterAction.Keys.First().Name;
+        else
+            return "Changement de position";
     }
 }
---
+++ Assets/Scripts/Combat/Attacks/Move.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using S_M_D.Spell;
+using UnityEngine;
+using UnityEngine.UI;
+using S_M_D.Character;
+
+public class MoveHero : MonoBehaviour
+{
+    void OnCLick()
+    {
+        if( BaseCombat.Attack.Target <= 0 && BaseCombat.Attack.Spell == null)
+        {
+            MoveAction();
+            BaseCombat.Combat.NextTurn();
+        }
+        Attacking.AfterAction();
+    }
+    public void MoveAction()
+    {
+        BaseHeros hero = BaseCombat.Combat.GetCharacterTurn() as BaseHeros;
+        BaseHeros hero2 = BaseCombat.Combat.Heros[BaseCombat.Attack.Target];
+
+        BaseCombat.Combat.SpellManager.MoveCharacter<BaseHeros>( GetPositionOfTheHero( hero ), BaseCombat.Attack.Target );
+    }
+
+    private int GetPositionOfTheHero( BaseHeros hero )
+    {
+        for (int i = 0; i < BaseCombat.Combat.Heros.Length; i++)
+        {
+            if (hero == BaseCombat.Combat.Heros[i])
+                return i;
+        }
+        return 0;
+    }
+}
