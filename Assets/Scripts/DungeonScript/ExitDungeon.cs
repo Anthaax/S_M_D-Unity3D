9dea1be826478117b985d3df6b31edbded627c7b
Benoit Humblot <benoit.humblot@orange.fr>
2016-06-28 16:53:33 +0200

    Implémentation du  boutonde sortie de donjon

---
+++ Assets/Scripts/DungeonScript/ExitDungeon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitDungeon : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene( 1 );
+            BaseCombat.Gtx.DungeonManager.EndOfTheDuengon( );
+            Start.Gtx = BaseCombat.Gtx;
+        }
+	}
+}
