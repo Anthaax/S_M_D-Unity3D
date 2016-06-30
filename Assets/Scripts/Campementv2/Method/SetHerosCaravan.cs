fae11210c018457f4c0138d29ee8b83101d3024d
Peuch <lucas.78.13@gmail.com>
2016-06-30 12:00:54 +0200

    Affichage heros dispo caravane selon son nombre de hérosdispo

--- Assets/Scripts/Campementv2/Method/SetHerosCaravan.cs
+++ Assets/Scripts/Campementv2/Method/SetHerosCaravan.cs
@@ -8,16 +8,38 @@ public class SetHerosCaravan : MonoBehaviour {
     
 	public void OnClick () {
         Caravan caravan = Start.Gtx.PlayerInfo.GetBuilding(S_M_D.Camp.Class.BuildingNameEnum.Caravan) as Caravan;
+        /*
         int x = 1;
-        Debug.Log("Nb : " + caravan.HerosDispo.Count);
+        //Debug.Log("Nb : " + caravan.HerosDispo.Count);
         foreach (BaseHeros heros in caravan.HerosDispo )
         {
+            GameObject.Find("HeroDispo" + x).SetActive(true);
             GameObject.Find("HeroDispo" + x + "T").GetComponent<Text>().text = heros.CharacterName;
             GameObject.Find("HeroDispo" + x + "Prix").GetComponent<Text>().text += heros.Price.ToString();
             if (heros.IsMale == true) GameObject.Find("HeroDispo" + x + "I").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + heros.CharacterClassName + "IconeM");
             else GameObject.Find("HeroDispo" + x + "I").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + heros.CharacterClassName + "IconeF");
             x++;
 
+        }
+        */
+        int x = 1;
+        for(x = 1; x < (caravan.HerosDispo.Count)+1; x++)
+        {
+                GameObject.Find("HeroDispo" + x).SetActive(true);
+                GameObject.Find("HeroDispo" + x + "T").GetComponent<Text>().text = caravan.HerosDispo[x-1].CharacterName;
+                GameObject.Find("HeroDispo" + x + "Prix").GetComponent<Text>().text += caravan.HerosDispo[x-1].Price.ToString();
+                if (caravan.HerosDispo[x-1].IsMale == true) GameObject.Find("HeroDispo" + x + "I").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + caravan.HerosDispo[x-1].CharacterClassName + "IconeM");
+                else GameObject.Find("HeroDispo" + x + "I").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Icones/" + caravan.HerosDispo[x-1].CharacterClassName + "IconeF");
+        }
+        while (x < 5)
+        {
+            GameObject.Find("HeroDispo" + x).SetActive(false);
+            /*
+            GameObject.Find("HeroDispo" + x + "T").GetComponent<Text>().text = "";
+            GameObject.Find("HeroDispo" + x + "Prix").GetComponent<Text>().text += "";
+            GameObject.Find("HeroDispo" + x + "I").GetComponent<Image>().sprite = null;
+            */
+            x += 1;
         }
     }
 }
