34efc781dbebcb05290083c445b940ad17723716
NicolasY <nicolasyazman@gmail.com>
2016-07-07 09:56:30 +0200

    Détruit CombatNetworkManager quand on arrête le combat.

--- Assets/Scripts/CombatV2/CombatLogic.cs
+++ Assets/Scripts/CombatV2/CombatLogic.cs
@@ -93,6 +93,8 @@ public class CombatLogic : NetworkBehaviour
     [ClientRpc]
     public void Rpc_ClientCombatEnd()
     {
+        if (GameObject.Find("CombatNetworkManager") != null)
+            Destroy(GameObject.Find("CombatNetworkManager").gameObject);
         SceneManager.LoadScene(2);
         BoardManager.Map = StartCombat.Map;
         BoardManager.Gtx = StartCombat.Gtx;
@@ -102,6 +104,8 @@ public class CombatLogic : NetworkBehaviour
     [Command]
     public void Cmd_ServerCombatEnd()
     {
+        if (GameObject.Find("CombatNetworkManager") != null)
+            Destroy(GameObject.Find("CombatNetworkManager").gameObject);
         SceneManager.LoadScene(2);
         BoardManager.Map = StartCombat.Map;
         BoardManager.Gtx = StartCombat.Gtx;
